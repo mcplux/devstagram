ad0a28a60cf0ac0b2bf9ef9e297d4d8e182f05a4
Juan Martinez <pablomar01@outlook.com>
2023-12-29 15:11:47 -0500

    logout added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,26 @@
         <div class="container mx-auto flex justify-between items-center">
           <h1 class="text-xl font-black">Devstagram</h1>
 
+          @auth
+          <nav class="flex gap-2 items-center">
+            <a class="font-bold text-gray-600 text-sm" href="#">
+              Hi <span class="font-normal">{{ auth()->user()->username }}</span>
+            </a>
+            <form method="POST" action="{{ route('logout') }}">
+              @csrf
+              <button type="submit" class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">
+                Logout
+              </button>
+            </form>
+          </nav>
+          @endauth
+
+          @guest
           <nav class="flex gap-2 items-center">
             <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('login') }}">Login</a>
             <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Signup</a>
           </nav>
+          @endguest
         </div>
       </header>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -24,5 +25,7 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
