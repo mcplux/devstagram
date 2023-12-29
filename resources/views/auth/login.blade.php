a7926d73eed8d4fcd8dc839f55ff43ba9cc651b6
Juan Martinez <pablomar01@outlook.com>
2023-12-29 14:38:40 -0500

    login view and controller creted

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('title')
+    Login on Devstagram
+@endsection
+
+@section('content')
+  <div class="md:flex md:justify-center md:gap-10 md:items-center">
+    <div class="md:w-1/2">
+      <img src="{{ asset('img/login.jpg') }}" alt="Login image">
+    </div>
+
+    <div class="md:w-1/3 bg-white p-6 rounded-lg shadow-lg">
+      <form novalidate>
+        @csrf
+        <div class="mb-5">
+          <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email"
+            class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+            value="{{ old('email') }}"
+            required
+          />
+          @error('email')
+          <p class="bg-red-500 text-white my-2 rounded-lg text-sm text-center p-2">
+            {{ $message }}
+          </p>
+          @enderror
+        </div>
+
+        <div class="mb-5">
+          <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Your password"
+            class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
+            required
+          />
+          @error('password')
+          <p class="bg-red-500 text-white my-2 rounded-lg text-sm text-center p-2">
+            {{ $message }}
+          </p>
+          @enderror
+        </div>
+
+        <input
+          type="submit"
+          class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+          value="Login"
+        />
+      </form>
+    </div>
+  </div>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -109,6 +109,7 @@
         <input
           type="submit"
           class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+          value="Sign up"
         />
       </form>
     </div>
